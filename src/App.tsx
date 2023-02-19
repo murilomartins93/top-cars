7d56f7a121d5839d6b36171ba44e0a98a16d3055
Murilo Martins <murilomartins@outlook.com>
2023-02-19 19:33:15 -0300

    :tada: Project created

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Test</h1>
+    </>
+  )
+}
+
+export default App
