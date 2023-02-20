2b999716c62580743d06d6b08afc900ff7c94df6
Murilo Martins <murilomartins@outlook.com>
2023-02-19 22:55:26 -0300

    :construction: Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+function Button() {
+  return <div className="tc-btn">Comprar</div>;
+}
+
+export default Button;
--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 import carImg from "../../assets/car-card.png";
+import Button from "../Button";
 
 function CatalogCard() {
   return (
@@ -12,6 +13,7 @@ function CatalogCard() {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Cupiditate, nisi
           </p>
+          <Button />
         </div>
       </div>
     </>
